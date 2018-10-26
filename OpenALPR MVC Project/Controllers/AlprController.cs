85abc1fbca00daa6ac3285a3854d7de3840d4f59
Grant Hair <grant.hair@arnoldclark.com>
2018-10-26 16:30:49 +0100

    initial commit

---
+++ OpenALPR MVC Project/Controllers/AlprController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Web.Http;
+using OpenALPR_MVC_Project.Models;
+
+namespace OpenALPR_MVC_Project.Controllers
+{
+    public class AlprController : ApiController
+    {
+        public RegPlateVm Post([FromBody]Request request)
+        {
+            var requestId = Guid.NewGuid();
+
+            var data = File.ReadAllBytes(request.ImageUrl);
+
+            using (var image = Image.FromStream(new MemoryStream(data)))
+            {
+                image.Save("output.png", ImageFormat.Png);  // Or Png
+            }
+
+            var results = OpenALPRHelper.Recognize(request.ImageUrl, "gb");
+
+            var model = new RegPlateVm { Reg = results.Plates[0].TopNPlates[0].Characters };
+
+            return model;
+        }
+    }
+}
---
+++ OpenALPR MVC Project/Models/Request.cs
@@ -0,0 +1,7 @@
+﻿namespace OpenALPR_MVC_Project.Models
+{
+    public class Request
+    {
+        public string ImageUrl { get; set; }
+    }
+}
